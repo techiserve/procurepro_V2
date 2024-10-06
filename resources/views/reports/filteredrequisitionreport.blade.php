9256afcab36f1a58386ba8cb20bc2f936d87c4bf
itaivincent <itaivincent321@gmail.com>
2024-10-06 21:53:26 +0200

    done

--- resources/views/reports/filteredrequisitionreport.blade.php
+++ resources/views/reports/filteredrequisitionreport.blade.php
@@ -8,7 +8,7 @@
             <div class="card">
                 <div class="card-header">
                     <strong> Requisition Summary</strong>
-                    <button class="btn btn-primary btn-sm pull-right"  data-toggle="modal" data-target="#filterModal" ><i class="fa fa-filter"></i> Filter Growers List</button>
+                    <button class="btn btn-primary btn-sm pull-right"  data-toggle="modal" data-target="#filterModal" ><i class="fa fa-filter"></i> Filter Requisition List</button>
                     <!-- <a href="/growers/" class="btn btn-primary btn-sm pull-right"><i style="color:white;" class="fa fa-align-justify"></i> Filter Requisitions</a> -->
                
                 </div>
