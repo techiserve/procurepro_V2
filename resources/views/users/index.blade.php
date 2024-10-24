fa5e1206f2f2c530567e9b093c8050c5bd50ab97
itaivincent <itaivincent321@gmail.com>
2024-10-24 12:20:24 +0200

    done

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,5 +1,9 @@
 @extends('stack.layouts.admin')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script> 
+<link href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css" rel="stylesheet">
 
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js"></script>
 @section('content')
 <div class="content-header row">
   <div class="content-header-left col-md-6 col-12 mb-2">
@@ -67,7 +71,22 @@
                   <td class="text-center">                        
                
                     <a href="/users/{{$user->id}}/edit" class="btn btn-icon btn-info mr-1"><i class="fa fa-pencil"></i> Edit</a> 
-                    <a href='#'class="btn btn-icon btn-danger mr-1"><i class="fa fa-trash"></i> Delete</a> &nbsp;
+                    <a href='#'class="btn btn-icon btn-danger mr-1"   onclick="
+                        event.preventDefault(); // Prevent the default link behavior
+                        Swal.fire({
+                            title: 'Delete User?',
+                            text: 'You won\'t be able to undo this!',
+                            icon: 'info', // Updated property for SweetAlert2
+                            showCancelButton: true,
+                            confirmButtonText: 'Continue',
+                            cancelButtonText: 'Cancel'
+                          }).then((result) => {
+                            if (result.isConfirmed) {
+                              // Redirect to the URL or perform an action
+                              window.location.href = '/users/{{$user->id}}/delete'; // Replace with your actual URL
+                            }
+                          })
+                      "><i class="fa fa-trash"></i> Delete</a> &nbsp;
                   </td>
                 </tr>
                     </tr>
