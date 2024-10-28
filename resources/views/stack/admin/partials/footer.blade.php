9ed06755775c58ecef37121edf80ff7092cd6f7e
itaivincent <itaivincent321@gmail.com>
2024-10-28 14:47:48 +0200

    forgot password

--- resources/views/stack/admin/partials/footer.blade.php
+++ resources/views/stack/admin/partials/footer.blade.php
@@ -3,6 +3,6 @@
 
    <!-- BEGIN: Footer-->
    <footer class="footer fixed-bottom footer-light navbar-border">
-        <p class="clearfix blue-grey lighten-2 text-sm-center mb-0 px-2"><span class="float-md-left d-block d-md-inline-block">APC &copy; <?php echo date('Y');?> <a class="text-bold-800 grey darken-2" href="#" target="_blank"> </a></span><span class="float-md-right d-none d-lg-block">Powered by <a href="#">TechIServe</a></span></p>
+        <p class="clearfix blue-grey lighten-2 text-sm-center mb-0 px-2"><span class="float-md-left d-block d-md-inline-block">APC &copy; <?php echo date('Y');?> <a class="text-bold-800 grey darken-2" href="#" target="_blank"> </a></span><span class="float-md-right d-none d-lg-block">Powered by <a href="#">techiserve.ai</a></span></p>
     </footer>
     <!-- END: Footer-->
