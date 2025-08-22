a3415e32983ffc35474bd73c271563ad5d11ac07
itaivincent <itaivincent321@gmail.com>
2025-08-22 09:57:21 +0200

    ui updates

--- resources/views/classifications/create.blade.php
+++ resources/views/classifications/create.blade.php
@@ -3,101 +3,108 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js"></script>
 @section('content')
-<div class="container-fluid">
-  <div class="animated fadeIn">
+<div class="body-content__header">
+    <ul>
+        <li><a href="#">Create Classification of Expense</a></li>
+    </ul>
+</div>
+
+<div class="body-content__wrapper">
     <div class="row">
-      <div class="col-sm-12">
-
-        <!-- Create Classification Form -->
-        <form method="POST" action="{{ route('classifications.store') }}">
-          @csrf
-          <div class="card">
-            <div class="card-header">
-              <strong>Create Classification of Expense</strong>
-            </div>
-
-            <div class="card-body">
-              @if (session('success'))
-                <div class="alert alert-success">{{ session('success') }}</div>
-              @endif
-
-              <div class="row">
-                <div class="col-sm-6">
-                  <div class="form-group">
-                    <label for="name">Classification Name</label>
-                    <input class="form-control" id="name" name="name" type="text" required>
-                  </div>
-                </div>
-                <div class="col-sm-6">
-                  <div class="form-group" style="margin-top: 40px;">
-                    <div class="checkbox">
-                      <label><input type="checkbox" name="active" checked> Active</label>
+        <div class="col-sm-12">
+            <form method="POST" action="{{ route('classifications.store') }}">
+                @csrf
+                <div class="card">
+                    <div class="card-header">
+                        <strong>Create Classification of Expense</strong>
                     </div>
-                  </div>
+
+                    <div class="card-body">
+                        @if (session('success'))
+                            <div class="alert alert-success">{{ session('success') }}</div>
+                        @endif
+
+                       <div class="row">
+                          <div class="col-md-6">
+                              <div class="form-col">
+                                  <label for="name">Classification Name</label>
+                                  <input class="form-control" id="name" name="name" type="text" placeholder="Enter Classification Name..." required>
+                              </div>
+                          </div>
+
+                          <div class="col-md-3 d-flex align-items-center">
+                              <div class="form-check form-switch">
+                                  <input class="form-check-input" type="checkbox" role="switch" name="active" value="1" id="flexSwitchActive" checked />
+                                  <label class="form-check-label" for="flexSwitchActive">Active</label>
+                              </div>
+                          </div>
+                      </div>
+                    </div>
+
+                 <div class="card-footer">
+                    <div class="d-flex justify-content-end gap-2">
+                        <input type="submit" class="btn btn-success" value="Save" style="padding:10px 20px; font-size:16px; min-width:100px;" />
+                        <input type="reset" class="btn btn-danger" value="Cancel" style="padding:10px 20px; font-size:16px; min-width:100px;" />
+                    </div>
                 </div>
-              </div>
-            </div>
-
-            <div class="card-footer">
-              <button type="submit" class="btn btn-primary pull-right">Save</button>
-            </div>
-          </div>
-        </form>
-
-        <br>
-
-        <!-- List of Classifications -->
-        <div class="card">
-          <div class="card-header">
-            <strong>All Classifications of Expense</strong>
-          </div>
-          <div class="card-body table-responsive">
-            <table class="table table-bordered table-striped">
-              <thead style="background-color:#000C3D; color:#fff;">
-                <tr>
-                  <th>Name</th>
-                  <th>Status</th>
-                  <th>Actions</th>
-                </tr>
-              </thead>
-              <tbody>
-                @forelse($classifications as $classification)
-                  <tr>
-                    <td>{{ $classification->name }}</td>
-                    <td>
-                      <span class="badge {{ $classification->active ? 'badge-success' : 'badge-danger' }}">
-                        {{ $classification->active ? 'Active' : 'Inactive' }}
-                      </span>
-                    </td>
-                    <td>
-                      <a href="{{ route('classifications.edit', $classification->id) }}" class="label label-sm label-success">
-                        <span class='fa fa-pencil'></span>
-                        <span class='hidden-sm hidden-sm hidden-md'> Edit</span>
-                      </a>
-
-                      <form action="{{ route('classifications.destroy', $classification->id) }}" method="POST" style="display:inline;" onsubmit="return confirm('Are you sure you want to delete this classification?');">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="label label-sm label-danger" style="border:none; background:none;">
-                          <span class='fa fa-trash'></span>
-                          <span class='hidden-sm hidden-sm hidden-md'> Delete</span>
-                        </button>
-                      </form>
-                    </td>
-                  </tr>
-                @empty
-                  <tr>
-                    <td colspan="3" class="text-center">No classifications found.</td>
-                  </tr>
-                @endforelse
-              </tbody>
-            </table>
-          </div>
+                </div>
+            </form>
         </div>
-
-      </div>
     </div>
-  </div>
+
+    <!-- Classification List Table -->
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="card">
+                <div class="card-header">
+                    <strong>All Classifications of Expense</strong>
+                </div>
+                <div class="card-body">
+                    <table class="table table-responsive-sm table-bordered table-striped table-sm">
+                        <thead>
+                            <tr>
+                                <th class="text-center">Name</th>
+                                <th class="text-center">Status</th>
+                                <th class="text-center">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @forelse($classifications as $classification)
+                                <tr>
+                                    <td class="text-center">{{ $classification->name }}</td>
+                                    <td class="text-center">
+                                        @if($classification->active)
+                                            <span class='badge badge-success'>Active</span>
+                                        @else
+                                            <span class='badge badge-secondary'>Inactive</span>
+                                        @endif
+                                    </td>
+                                    <td class="text-center">
+                                        <a href="{{ route('classifications.edit', $classification->id) }}" class='btn btn-info btn-sm'>
+                                            <span class='fa fa-pencil'></span> Edit
+                                        </a>
+
+                                        <form action="{{ route('classifications.destroy', $classification->id) }}" method="POST" style="display:inline;" 
+                                            onsubmit="return confirm('Are you sure you want to delete this classification?');">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class='btn btn-danger btn-sm'>
+                                                <span class='fa fa-trash'></span> Delete
+                                            </button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @empty
+                                <tr>
+                                    <td colspan="3" class="text-center">No classifications found.</td>
+                                </tr>
+                            @endforelse
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
 
--- resources/views/classifications/edit.blade.php
+++ resources/views/classifications/edit.blade.php
@@ -1,52 +1,57 @@
 @extends('html.default')
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+
 @section('content')
-<div class="container-fluid">
-  <div class="animated fadeIn">
+<div class="body-content__header">
+    <ul>
+        <li><a href="#">Update Classification of Expense</a></li>
+    </ul>
+</div>
+
+<div class="body-content__wrapper">
     <div class="row">
-      <div class="col-sm-12">
+        <div class="col-sm-12">
 
-        <!-- Edit Classification Form -->
-        <form method="POST" action="{{ route('classifications.update', $classification->id) }}">
-          @csrf
-          @method('PUT')
-          <div class="card">
-            <div class="card-header">
-              <strong>Update Classification of Expense</strong>
-            </div>
+            <!-- Edit Classification Form -->
+            <form method="POST" action="{{ route('classifications.update', $classification->id) }}">
+                @csrf
+                @method('PUT')
 
-            <div class="card-body">
-              <div class="row">
-                <div class="col-sm-6">
-                  <div class="form-group">
-                    <label for="name">Classification Name</label>
-                    <input type="text" class="form-control" id="name" name="name" value="{{ $classification->name }}" required>
-                  </div>
-                </div>
-
-                <div class="col-sm-6">
-                  <div class="form-group" style="margin-top: 34px;">
-                    <div class="checkbox">
-                      <label>
-                        <input type="checkbox" name="active" value="1" {{ $classification->active ? 'checked' : '' }}> Active
-                      </label>
+                <div class="card">
+                    <div class="card-header">
+                        <strong>Update Classification of Expense</strong>
                     </div>
-                  </div>
+
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-col">
+                                    <label for="name">Classification Name</label>
+                                    <input type="text" class="form-control" id="name" name="name" placeholder="Enter Classification Name..." value="{{ $classification->name }}" required>
+                                </div>
+                            </div>
+
+                               <div class="col-md-3 d-flex align-items-center">
+                                <div class="form-check form-switch">
+                                    <input class="form-check-input" type="checkbox" role="switch" name="active" value="1" id="flexSwitchActiveEdit" {{ $classification->active ? 'checked' : '' }} />
+                                    <label class="form-check-label" for="flexSwitchActiveEdit">Active</label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="card-footer">
+                        <div class="d-flex justify-content-end gap-2">
+                            <button type="submit" class="btn btn-success" style="padding:10px 20px; font-size:16px; min-width:100px;">Update</button>
+                            <a href="{{ route('classifications.create') }}" class="btn btn-danger" style="padding:10px 20px; font-size:16px; min-width:100px;">Back</a>
+                        </div>
+                    </div>
                 </div>
-              </div>
-            </div>
 
-            <div class="card-footer">
-              <button type="submit" class="btn btn-success">Update</button>
-              <a href="{{ route('classifications.create') }}" class="btn btn-secondary">Back</a>
-            </div>
-          </div>
-        </form>
+            </form>
 
-      </div>
+        </div>
     </div>
-  </div>
 </div>
 @endsection
-
