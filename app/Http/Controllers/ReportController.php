7e0d9384121592a82fb4e685e10dcc6084440e81
itaivincent <itaivincent321@gmail.com>
2025-08-28 14:57:01 +0200

    gdsj

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -475,8 +475,10 @@ public function filter(Request $request)
             ->table('vpms_purchase_requisition') // <-- replace with your table name
             ->where('unique_id', 'like', $suffix.'%')
             ->orderByRaw('CAST(SUBSTRING_INDEX(unique_id, "-", -1) AS UNSIGNED)') // natural numeric order
+            ->limit(50)
             ->get();
-
+            
+         //   dd($requisitions);
 
              return view('reports.procureprorequisition', compact('requisitions'));
 
